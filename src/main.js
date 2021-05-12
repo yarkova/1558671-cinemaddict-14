2ffdcd8e0b4260c8665898608de53d2363edef55
yarkova <mariya-yarkova@mail.ru>
2021-05-12 13:10:13 +0300

    Революция или эволюция? (часть 2)

--- src/main.js
+++ src/main.js
@@ -9,36 +9,34 @@ import Popup from './view/popup.js';
 import FilmsEmpty from './view/films-empty.js';
 
 import {generateFilms} from './mock/film.js';
-import {renderTemplate, renderElement, RenderPosition} from './common/utils.js';
+import {renderTemplate, render, RenderPosition} from './common/utils.js';
 import {getRandomInteger, getRandomFloat} from './common/utils.js'
 
 const films = generateFilms();
 console.log(films);
 
 
-const render = (container, template, place) => {
-  container.insertAdjacentHTML(place, template);
-}
+
 
 const siteMainElement = document.querySelector('.main');
-renderElement (siteMainElement,  new SiteMenu().getElement(), RenderPosition.BEFOREEND);
+render (siteMainElement,  new SiteMenu().getElement(), RenderPosition.BEFOREEND);
 
 
 
 //onst statsElement = document.querySelector('body');
-//renderElement (statsElement, new Stats().getElement(), RenderPosition.BEFOREEND);
+//render (statsElement, new Stats().getElement(), RenderPosition.BEFOREEND);
 
 
 const headerElement = document.querySelector('.header');
-renderElement (headerElement, new Profile().getElement(), RenderPosition.BEFOREEND);
+render (headerElement, new Profile().getElement(), RenderPosition.BEFOREEND);
 
 
 const sortElement = document.querySelector('.main');
-renderElement (sortElement, new Sort().getElement(), RenderPosition.BEFOREEND);
+render (sortElement, new Sort().getElement(), RenderPosition.BEFOREEND);
 
 
 const filmsElement = document.querySelector('.main');
-renderElement (filmsElement, new Films(films).getElement(), RenderPosition.BEFOREEND);
+render (filmsElement, new Films(films).getElement(), RenderPosition.BEFOREEND);
 
 const ClickItems = document.querySelectorAll('.film-card__title, .film-card__poster, .film-card__comments');
 for (let element of ClickItems) {
@@ -56,7 +54,7 @@ for (let element of ClickItems) {
     newPopup.getElement().querySelector('.film-details__close-btn').onclick = function () {
       popupElement.removeChild(newPopup.getElement());
     }
-    renderElement (popupElement, newPopup.getElement(), RenderPosition.BEFOREEND);
+    render (popupElement, newPopup.getElement(), RenderPosition.BEFOREEND);
     document.addEventListener('keydown', onEscKeyDown);
   }
 
@@ -70,7 +68,7 @@ for (let element of ClickItems) {
 //filmsEmptyElement, new FilmsEmpty().getElement(), RenderPosition.BEFOREEND
 
 // const filmFilterElement = document.querySelector('.main');
-// renderElement (filmFilterElement, new FilmFilter().getElement(), RenderPosition.BEFOREEND);
+// render (filmFilterElement, new FilmFilter().getElement(), RenderPosition.BEFOREEND);
 
 const footerElement = document.querySelector('.footer');
-renderElement (footerElement, new Footer().getElement(), RenderPosition.BEFOREEND);
+render (footerElement, new Footer().getElement(), RenderPosition.BEFOREEND);
